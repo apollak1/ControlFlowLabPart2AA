4fb704f01a7aaa344abcb359bb872dbb2fffe5af
Andrew Pollak <79533175+apollak1@users.noreply.github.com>
2024-11-12 11:14:40 -0600

    Update main.swift

--- ControlFlowLabPart2A/main.swift
+++ ControlFlowLabPart2A/main.swift
@@ -67,10 +67,8 @@ print("\n")
 
 print("***PROBLEM 2.0 : IF STATEMENT CONTROL FLOW")
 // Imagine you're creating a machine that will count your money for you and tell you how wealthy you are based on how much money you have. A variable dollars has been given to you with a value of 0. Write an if statement that prints "Sorry, kid. You're broke!" if dollars has a value of 0. Observe what is printed to the console.
-var dollars = 0
-if (dollars == 0) {
-    print("Sorry, kid. You're broke!")
-}
+
+
 
 print("\n")
 print("\n")
